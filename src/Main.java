5f127cb676654df5e46a3e05b979209b41263cbd
Ahmed Ashref <ahmed.ashref.sw@gmail.com>
2022-09-19 22:50:30 +0200

    setup and sol1929

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        int nums [] = {1,3,2,1};
+//        int [] res = Solution1929.getConcatenation(nums);
+//        System.out.println(Arrays.toString(res));
+    }
+}
